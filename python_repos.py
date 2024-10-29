28d54b33ae49ec47fda7019eee59451492963bee
Ziyike1 <tul51428@temple.edu>
2024-10-29 10:38:56 +0800

    visualize all data from github api

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,5 @@
 import requests
+import plotly.express as px
 
 #执行api调用并查看响应
 url = 'https://api.github.com/search/repositories'
@@ -11,27 +12,45 @@ print(f"Status code: {r.status_code}")
 #将响应转换为字典
 response_dict = r.json()
 # print(response_dict.keys())
-
-print(f"Total repositories: {response_dict['total_count']}")
-print(f"Complete results: {not response_dict['incomplete_results']}")
+# print(f"Total repositories: {response_dict['total_count']}")
+# print(f"Complete results: {not response_dict['incomplete_results']}")
 
 #搜索有关仓库的信息
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+# print(f"Repositories returned: {len(repo_dicts)}")
 
 #研究第一个仓库
-repo_dict = repo_dicts[0]
+# repo_dict = repo_dicts[0]
 # print(f"\nKeys: {len(repo_dict)}")
 # for key in sorted(repo_dict.keys()):
 #     print(key)
 
-print("\nSelected information about each repository:")
-for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Created: {repo_dict['created_at']}")
-    print(f"Updated: {repo_dict['updated_at']}")
-    print(f"Description: {repo_dict['description']}")
+#打印仓库的具体信息
+# print("\nSelected information about each repository:")
+# for repo_dict in repo_dicts:
+#     print(f"Name: {repo_dict['name']}")
+#     print(f"Owner: {repo_dict['owner']['login']}")
+#     print(f"Stars: {repo_dict['stargazers_count']}")
+#     print(f"Repository: {repo_dict['html_url']}")
+#     print(f"Created: {repo_dict['created_at']}")
+#     print(f"Updated: {repo_dict['updated_at']}")
+#     print(f"Description: {repo_dict['description']}")
 
+repo_names, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+    #创建悬停文本
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+
+#可视化
+title = "Most-Starred Python Projects"
+labels = {'x':'Repositories', 'y':'Stars'}
+fig = px.bar(x=repo_names, y=stars, title=title, labels=labels, hover_name=hover_texts)
+fig.update_layout(title_font_size=20, xaxis_title_font_size=10, yaxis_title_font_size=10)
+fig.show()
